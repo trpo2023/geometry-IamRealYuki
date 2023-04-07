46d3e829915cb04c4a208904b9ecca656b92c367
Efimov <IamRealYuki@users.noreply.github.com>
2023-04-07 23:57:23 +0700

    Codestyle improovements

--- src/libgeometry/geometry.h
+++ src/libgeometry/geometry.h
@@ -1,9 +1,9 @@
 #ifndef geometry
 
-int circle(char *str);
+int circle(char* str);
 
-double perimeter(char *str);
+double perimeter(char* str);
 
-double area(char *str);
+double area(char* str);
 
 #endif
